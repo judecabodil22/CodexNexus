c24a53dafbb90551f761ada80fe78a68b604219f
judecabodil22 <judecabodil22@gmail.com>
2024-06-11 19:45:32 +0800

    Created Web App

---
+++ Web Applications/Budgetary App Final Edition/Models/Bills.cs
@@ -0,0 +1,7 @@
+﻿namespace Budgetary_App_Final_Edition.Models
+{
+    public class Bills
+    {
+        public int MyProperty { get; set; }
+    }
+}
---
+++ Web Applications/Budgetary App Final Edition/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Budgetary_App_Final_Edition.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
