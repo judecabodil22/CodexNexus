115a36cdb79e6f89c5a571f50719252ddcf2f4d2
Justin <judecabodil22@gmail.com>
2025-10-21 11:11:35 +0800

    Added full CRUD functionality

--- Web Applications/BudgetMate/Models/Expense.cs
+++ Web Applications/BudgetMate/Models/Expense.cs
@@ -1,12 +1,23 @@
-﻿namespace BudgetMate.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BudgetMate.Models
 {
     public class Expense
     {
+        [Key]
         public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
         public string Category { get; set; }
+        [MaxLength(255)]
         public string Description { get; set; }
-        public string Amount { get; set; }
-        public DateTime Date { get; set; }
+
+        [Required]
+        public decimal Amount { get; set; }
+
+        [Required]
+        public DateTime Date { get; set; } = DateTime.Now;
 
     }
 }
